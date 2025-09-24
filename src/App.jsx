f1e59490747baa20c700df2745a136286496e30b
Sara Salzano <sarasalzano95@gmail.com>
2025-09-24 16:43:23 +0200

    esegui menu

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,35 @@ export default function MyApp() {
         </div>
     </header>
     <main>
-      <section className="Content"></section>
-      <section className="Menu"></section>
+      <section className="Content">
+        <p> -- Content goes here -- </p>
+      </section>
+      <section className="Menu">
+        <div className="MainWrapper">
+                <div className="ImgWrapper">
+                <img src="./src/assets/img/buy-comics-digital-comics.png" alt="comics icon" />
+                <span>DIGITAL COMICS</span>
+                </div>
+                <div className="ImgWrapper">
+                <img src="./src/assets/img/buy-comics-merchandise.png" alt="tshirt icon" />
+                <span>DC MERCHANDISE</span>
+                </div>
+
+                <div className="ImgWrapper">
+                <img src="./src/assets/img/buy-comics-subscriptions.png" alt="subscription" />
+                <span>SUBSCRIPTION</span>
+                </div>
+
+                <div className="ImgWrapper">
+                <img src="./src/assets/img/buy-comics-shop-locator.png" alt="shop locator" />
+                <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div className="ImgWrapper">
+                <img src="./src/assets/img/buy-dc-power-visa.svg" className="MenuSvg" alt="payment icon" />
+                <span>DC POWER ERA</span>
+                </div>
+            </div>
+      </section>
     </main>
     <footer>
         <section className="MainFooter"></section>
