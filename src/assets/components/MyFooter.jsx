894a8edea7b201277e7ac4461ee4c7c4cced1846
Sara Salzano <sarasalzano95@gmail.com>
2025-09-24 21:07:16 +0200

    esegui footer secondario

--- src/assets/components/MyFooter.jsx
+++ src/assets/components/MyFooter.jsx
@@ -1,6 +1,26 @@
 export default function MyFooter(){
     return (<footer>
-        <section className="MainFooter"></section>
-        <section className="SecondaryFooter"></section>
+        <section className="MainFooter">
+            <div className="MainFooterWrapper">
+                <div className='ColumnLeft'></div>
+                <div className='ColumnRight'></div>
+            </div>
+        </section>
+        <section className="SecondaryFooter">
+            <div className='SecondaryFooterWrapper'>
+                <div className='SecondaryFooterLeft'>
+                    <button>SIGN-UP NOW!</button>
+                </div>
+                <div className='SecondaryFooterRight'>
+                    <span>FOLLOW US</span>
+                    <img src="./src/assets/img/footer-facebook.png" alt="facebook icon" />
+                    <img src="./src/assets/img/footer-twitter.png" alt="twitter icon" />
+                    <img src="./src/assets/img/footer-youtube.png" alt="youtube icon" />
+                    <img src="./src/assets/img/footer-pinterest.png" alt="pinterest icon" />
+                    <img src="./src/assets/img/footer-periscope.png" alt="periscope icon" />
+                </div>
+            
+            </div>
+        </section>
     </footer>)
 }
