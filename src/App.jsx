84454358a6f2cb31e92e52ac172f471965c17381
Sara Salzano <sarasalzano95@gmail.com>
2025-09-24 16:18:13 +0200

    esegui header

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,42 @@ export default function MyApp() {
   return (
     <>
     <header>
+           <div className="HeaderWrapper">
+        <div className="LogoWrapper">
+            <img src="./src/assets/img/dc-logo.png" alt="logo dc" className='LogoHeader'></img>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#" className='BlueText'>COMICS</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
     </header>
     <main>
       <section className="Content"></section>
--- src/assets/components/MyHeader.jsx
+++ src/assets/components/MyHeader.jsx
@@ -1,5 +1,40 @@
 export default function MyHeader() {
     return (<header> 
-        <div className="SubHeader"></div>
+        <div className="HeaderWrapper">
+        <div className="LogoWrapper">
+            <img src="./src/assets/img/dc-logo.png" alt="logo dc" className="LogoHeader"></img>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#" className="BlueText">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
     </header>)
 }
